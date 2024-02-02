8426f092ee874db2971141f9e670d83f2fc306e2
Dina <515lincolnstreet@gmail.com>
2024-02-02 07:15:24 -0500

    First commit

--- src/test/java/com/loop/utilities/utilities/ExcelUtils.java
+++ src/test/java/com/loop/utilities/utilities/ExcelUtils.java
@@ -1,48 +1,48 @@
-package com.loop.utilities.utilities;
-
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.poi.ss.usermodel.*;
-
-import java.io.FileInputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-
-public class ExcelUtils {
-    private Sheet workSheet;
-    private Workbook workBook;
-    private String path;
-
-    private static final Logger LOG = LogManager.getLogger();
-    public ExcelUtils (String path,String sheetName ){
-        this.path = path;
-        try {
-            // Open Excel file
-            FileInputStream ExcelFile = new FileInputStream(path);
-            // Access the required test data sheet
-            workBook = WorkbookFactory.create(ExcelFile);
-            workSheet = (Sheet) workBook.getSheet(sheetName);
-        } catch (Exception e){
-            LOG.error(e.getMessage());
-            throw new RuntimeException();
-        }
-    }
-
-    public String getCellData(int rowNum, int colNum) {
-        Cell cell;
-        try {
-            cell = workSheet.getRow(rowNum).getCell(colNum);
-            String cellData = cell.toString();
-            return cellData;
-        } catch (Exception e) {
-            LOG.error(e.getMessage());
-            throw new RuntimeException();
-        }
-
-
+//package com.loop.utilities.utilities;
+//
+//
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+//import org.apache.poi.ss.usermodel.*;
+//
+//import java.io.FileInputStream;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Map;
+//
+//
+//public class ExcelUtils {
+//    private Sheet workSheet;
+//    private Workbook workBook;
+//    private String path;
+//
+//    private static final Logger LOG = LogManager.getLogger();
+//    public ExcelUtils (String path,String sheetName ){
+//        this.path = path;
+//        try {
+//            // Open Excel file
+//            FileInputStream ExcelFile = new FileInputStream(path);
+//            // Access the required test data sheet
+//            workBook = WorkbookFactory.create(ExcelFile);
+//            workSheet = (Sheet) workBook.getSheet(sheetName);
+//        } catch (Exception e){
+//            LOG.error(e.getMessage());
+//            throw new RuntimeException();
+//        }
+//    }
+//
+//    public String getCellData(int rowNum, int colNum) {
+//        Cell cell;
+//        try {
+//            cell = workSheet.getRow(rowNum).getCell(colNum);
+//            String cellData = cell.toString();
+//            return cellData;
+//        } catch (Exception e) {
+//            LOG.error(e.getMessage());
+//            throw new RuntimeException();
+//        }
+//
+//
 //        public List<Map<String, String>> getDataListV2 () {
 //            List<Map<String, String>> data = new ArrayList<>();
 //            List<String> columns = getColumnsNames();
@@ -89,7 +89,5 @@ public class ExcelUtils {
 //            }
 //            return data;
 //        }
-
-
-    }
-}
+//    }
+//}
--- src/test/resources/features/softAssertions.feature
+++ src/test/resources/features/softAssertions.feature
@@ -1,13 +1,13 @@
 Feature: Practice soft assertions
+
+  @soft
   Scenario: Soft assertions practice
     Given user is on Docuport login page
     Then user validates "Login" text is displayed
-    And user validates "forgot password" text is displayed
     When user enters credentials
       | username | b1g1_client@gmail.com |
       | password | Group1|
-    And user validates "choose account" text is displayed
-    Then user chooses account from drop down
-    And user clicks "continue" button
+    And user validates "Choose account" text is displayed
     And user clicks "home" button
-    And user clicks "invitations" button
+    And user clicks "Invitations" button
+    And user validates all assertions
